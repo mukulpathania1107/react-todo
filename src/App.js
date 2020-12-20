d77cd062b2dd534d827d2a2b1cc436f8b25c70d3
Mukul Pathania <mukulpathania96@gmail.com>
2020-12-20 16:49:44 +0530

    Add todos list state to the application
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
+      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
+      <TodoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,20 @@
 import './Form.css';
 
-const Form = () => {
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
+    const inputTextHandler = e => {
+        setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = e => {
+        e.preventDefault();
+        setTodos([...todos, { text: inputText, completed: false, id: Math.random() * 10000 }]);
+        setInputText('');
+    };
+
     return (
         <form>
-            <input type="text" className="todo-input" />
-            <button type="submit" className="todo-button">
+            <input onChange={inputTextHandler} type="text" value={inputText} className="todo-input" />
+            <button onClick={submitTodoHandler} type="submit" className="todo-button">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
