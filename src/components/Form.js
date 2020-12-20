26f39f8abe8e386dced9189ba6b214e85598807f
Mukul Pathania <mukulpathania96@gmail.com>
2020-12-20 15:58:09 +0530

    Add Form component and some styling for it
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

---
+++ src/components/Form.js
@@ -0,0 +1,22 @@
+import './Form.css';
+
+const Form = () => {
+    return (
+        <form>
+            <input type="text" className="todo-input" />
+            <button type="submit" className="todo-button">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+}
+
+
+export default Form;
